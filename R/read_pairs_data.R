279014067b006dc9a9d361b1946eb29c3d2fe055
Dicko Ahmadou <dicko.ahmadou@gmail.com>
2013-03-30 12:44:26 +0000

    faster read_pairs_data

--- R/read_pairs_data.R
+++ R/read_pairs_data.R
@@ -1,28 +1,14 @@
-##' Read pairs data given the pathg
+##' Read pairs data given the path
 ##'
 ##' @export
 ##' @param file
 ##' @author ahmadou
 read_pairs_data <- function(file) {
-    dat <- readLines(file)[-1]
-    dat <- as.list(dat)
-    idtype <- sapply(dat, function(x)
-                     strsplit(x, ",")[[1]][1])
-    dat <- lapply(dat, function(x)
-                  strsplit(x, ", ")[[1]][-1])
-    dat <- lapply(dat, strsplit, " ")
-    dat <- lapply(dat, function(x)
-                  sapply(x, as.numeric)
-                  )
-
-    nrep <- sapply(dat, nrow)
-    sampleid <- rep(seq_len(length(dat)), nrep)
-
-    dat <- do.call("rbind", dat)
-    dat <- cbind(sampleid, dat)
-    colnames(dat)[2:3] <- c("A", "B")
-    dat <- as.data.frame(dat)
-    dat$sampleid <- as.character(factor(dat$sampleid, labels = idtype))
-    dat
+    raw <- fread(file)
+    setkey(raw, SampleID)
+    raw[, list(A = clean_data(A), B = clean_data(B)), by = "SampleID"]
 }
 
+
+clean_data <- function(x)
+    as.numeric(unlist(strsplit(gsub("^\\s+", "", x), " ")))
